ff9ffa76754c9477a56a3cc075af7179fbea8505
usuario <mpardos@campusdigitalfp.com>
2026-04-30 13:22:36 +0200

    atras

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,7 @@
-<?php
-    $isInVistas = strpos($_SERVER['PHP_SELF'], '/vistas/') !== false;
-    $urlPrefix = $isInVistas ? '../' : '';
-?>
 <header class="main-header">
     <section class="container">
         <section class="icon">
-            <img src="<?php echo $urlPrefix; ?>assets/img/logo.png" alt="logo barberia">
+            <img src="../assets/img/logo.png" alt="logo barberia">
         </section>
         <section class="logo">
             <span class="text-white">BARBERÍA</span> 
@@ -14,17 +10,17 @@
 
         <nav class="nav-menu">
             <ul>
-                <li><a href="<?php echo $urlPrefix; ?>vistas/servicios.php">SERVICIOS</a></li>
-                <li><a href="<?php echo $urlPrefix; ?>vistas/equipo.php">EQUIPO</a></li>
-                <li><a href="<?php echo $urlPrefix; ?>vistas/galeria.php">GALERÍA</a></li>
-                <li><a href="<?php echo $urlPrefix; ?>vistas/reseñas.php">RESEÑAS</a></li>
-                <li><a href="<?php echo $urlPrefix; ?>vistas/ubicacion.php">UBICACIÓN</a></li>
-                <li><a href="<?php echo $urlPrefix; ?>vistas/blog.php">BLOG</a></li>
+                <li><a href="vistas/servicios.php">SERVICIOS</a></li>
+                <li><a href="vistas/equipo.php">EQUIPO</a></li>
+                <li><a href="vistas/galeria.php">GALERÍA</a></li>
+                <li><a href="vistas/reseñas.php">RESEÑAS</a></li>
+                <li><a href="vistas/ubicacion.php">UBICACIÓN</a></li>
+                <li><a href="vistas/blog.php">BLOG</a></li>
             </ul>
         </nav>
 
         <section class="cta">
-            <a href="<?php echo $urlPrefix; ?>vistas/reservas.php" class="btn-reserve">RESERVAR</a>
+            <a href="../vistas/reservas.php" class="btn-reserve">RESERVAR</a>
         </section>
     </section>
 </header>
