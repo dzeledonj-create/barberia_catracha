42d107484a94e11d18e8bb9e7a656f4856258383
pablo <plopezm@campusdigitalfp.com>
2026-06-02 08:42:54 +0200

    comentario phpunit

--- api/tests/ServicioTest.php
+++ api/tests/ServicioTest.php
@@ -4,10 +4,13 @@ use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . '/../Clases/Servicio.php';
 
+//extends TestCase es necesario para que PHPUnit reconozca esta clase como una clase de prueba
 class ServicioTest extends TestCase
 {
+    // Prueba para verificar que se puede crear un nuevo servicio correctamente
     public function testCrearServicio()
     {
+        // Crear un nuevo servicio con datos de prueba
         $servicio = new Servicio(
             "Corte Test",
             "Descripción test",
@@ -17,6 +20,7 @@ class ServicioTest extends TestCase
             "Corte"
         );
 
+        //Para verificar que las propiedades del servicio se asignaron correctamente
         $this->assertEquals("Corte Test", $servicio->nombre);
         $this->assertEquals("Descripción test", $servicio->descripcion);
         $this->assertEquals(15, $servicio->precio);
