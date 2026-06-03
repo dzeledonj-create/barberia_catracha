4cba173005e53802a5af26d26fcd8aa64f965c29
dzeledonj-create <dzeledonj@campusdigitalfp.com>
2026-06-03 11:38:37 +0200

    cambio de rutas

--- api/vistas/equipo.php
+++ api/vistas/equipo.php
@@ -10,8 +10,8 @@ $barberos = Barbero::obtenerActivos();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Equipo - Barbería Catracha</title>
-    <link rel="stylesheet" href="../../assets/style.css">
-    <link rel="icon" href="../../assets/img/logo.png" type="image/png">
+    <link rel="stylesheet" href="../../../assets/style.css">
+    <link rel="icon" href="../../../assets/img/logo.png" type="image/png">
 </head>
 <body>
 
@@ -34,7 +34,7 @@ $barberos = Barbero::obtenerActivos();
     <section class="barbero-card">
 
         <section class="barbero-img">
-            <img src="../../assets/img/<?= htmlspecialchars($barbero['foto_url']) ?>" 
+            <img src="../../../assets/img/<?= htmlspecialchars($barbero['foto_url']) ?>" 
                  alt="<?= htmlspecialchars($barbero['nombre']) ?>">
         </section>
 
@@ -64,9 +64,9 @@ $barberos = Barbero::obtenerActivos();
 
         </section>
 
-        <footer class="equipo-cta">
-            <a href="../../vistas/reservas.php" class="btn-reservar-equipo">ELIGE TU BARBERO Y RESERVA</a>
-        </footer>
+        <section class="equipo-cta">
+            <a href="/vistas/reservas.php" class="btn-reservar-equipo">ELIGE TU BARBERO Y RESERVA</a>
+        </section>
     </section>
 </main>
 
